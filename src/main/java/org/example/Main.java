839057112f4e569fcf722af4fe21084f8ebf9f20
erenbzbl <erenbozbel@gmail.com>
2024-12-13 00:51:35 +0300

    Java101 Değer Tipleri ve Değişkenler

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        byte numberOne = 1;
+        short numberTwo = 2;
+        int numberThree = 3;
+        long numberFour = 4L;
+        float numberFive = 5.0f;
+        double numberSix = 6.0;
+        boolean numberSeven = true;
+        boolean numberEight = false;
+
+        int numberNine;
+        int numberTen;
+
+        numberNine = 9;
+        numberTen = 10;
+
+        System.out.println(numberNine + numberTen);
+
+    }
+}
