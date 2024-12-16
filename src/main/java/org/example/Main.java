5b3d812d9358f27568c37cc167a7a6f11d81b299
erenbzbl <erenbozbel@gmail.com>
2024-12-16 18:24:16 +0300

    Boy kilo Endeksi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,37 @@
-package org.example;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        byte numberOne = 1;
-        short numberTwo = 2;
-        int numberThree = 3;
-        long numberFour = 4L;
-        float numberFive = 5.0f;
-        double numberSix = 6.0;
-        boolean numberSeven = true;
-        boolean numberEight = false;
+        double  boy;
+        double  kilo;
+        double  sonuc;
 
-        int numberNine;
-        int numberTen;
+        Scanner scanner = new Scanner(System.in);
 
-        numberNine = 9;
-        numberTen = 10;
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kilo = scanner.nextDouble();
 
-        System.out.println(numberNine + numberTen);
+        System.out.print("Lütfen Boyunuzu Giriniz: ");
+        boy = scanner.nextDouble();
 
+        sonuc = kilo / (boy * boy);
+
+        if (sonuc <= 18.5){
+            System.out.println("Zayıf: " + sonuc);
+        }
+        else if (sonuc >= 18.5 && sonuc <= 24.9){
+            System.out.println("Normal Kilolu " + sonuc);
+        }
+        else if (sonuc >= 24.9 && sonuc <= 29.9){
+            System.out.println("Fazla Kilolu " + sonuc);
+        }
+        else if (sonuc >= 29.9 && sonuc <= 34.9){
+            System.out.println("Obez " + sonuc);
+        }
+        else if (sonuc >= 34.9 && sonuc <= 39.9){
+            System.out.println("Aşırı Obez " + sonuc);
+        }
+        else if (sonuc >= 39.9 && sonuc >= 40.0){
+            System.out.println("Morbid Obez " + sonuc);
+        }
     }
 }
