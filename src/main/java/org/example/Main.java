2777acec350a99fb49a06a67348eea899dbc9935
erenbzbl <erenbozbel@gmail.com>
2024-12-20 01:02:05 +0300

    Calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,28 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        double armut = 2.14;
-        double elma = 3.67;
-        double domates = 1.11;
-        double muz = 0.95;
-        double patlican = 5.00;
+       int  n1, n2, select;
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("İlk Sayıyı Giriniz: ");
+       n1 = scanner.nextInt();
+       System.out.print("İkinci Sayıyı Giriniz: ");
+       n2 = scanner.nextInt();
+       System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+       System.out.print("Seçiminizi Giriniz: ");
+       select = scanner.nextInt();
 
-        double armutKilo, elmaKilo, domatesKilo, muzKilo, patlicanKilo;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Armut Kaç Kilo?: ");
-        armutKilo = scanner.nextDouble();
-
-        System.out.print("Elma Kaç Kilo?: ");
-        elmaKilo = scanner.nextDouble();
-
-        System.out.print("Domates Kaç Kilo?: ");
-        domatesKilo = scanner.nextDouble();
-
-        System.out.print("Muz Kaç Kilo?: ");
-        muzKilo = scanner.nextDouble();
-
-        System.out.print("Patlıcan Kaç Kilo: ");
-        patlicanKilo = scanner.nextDouble();
-
-        double toplamTutar = (armutKilo * armut) +
-                (elmaKilo * elma) +
-                (domatesKilo * domates) +
-                (muzKilo * muz) +
-                (patlicanKilo * patlican);
-        
-        System.out.println("Toplam: " + toplamTutar + " TL");
+       switch (select) {
+           case 1:
+               System.out.println("Toplama İşleminin Sonucu = " + n1 + " + " + n2 + " = " + (n1 + n2));
+               break;
+               case 2:
+                   System.out.println("Çıkarma İşleminin Sonucu = " + n1 + " - " + n2 + " = " + (n1 - n2));
+                   break;
+                   case 3:
+                       System.out.println("Çarpma İşleminin Sonucu = " + n1 + " * " + n2 + " = " + (n1 * n2));
+                       break;
+                       case 4:
+                           System.out.println("Bölme İşleminin Sonucu = " + n1 + " / " + n2 + " = " + (n1 / n2));
+       }
     }
 }
