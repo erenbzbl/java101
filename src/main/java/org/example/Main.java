21d96b43a7fe2bc44c0df2657b28abccbb3732b5
erenbzbl <erenbozbel@gmail.com>
2024-12-20 01:17:53 +0300

    Login Password

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,18 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       int  n1, n2, select;
+       String username, password;
        Scanner scanner = new Scanner(System.in);
-       System.out.print("İlk Sayıyı Giriniz: ");
-       n1 = scanner.nextInt();
-       System.out.print("İkinci Sayıyı Giriniz: ");
-       n2 = scanner.nextInt();
-       System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
-       System.out.print("Seçiminizi Giriniz: ");
-       select = scanner.nextInt();
+       System.out.print("Username: ");
+       username = scanner.nextLine();
+       System.out.print("Password: ");
+       password = scanner.nextLine();
 
-       switch (select) {
-           case 1:
-               System.out.println("Toplama İşleminin Sonucu = " + n1 + " + " + n2 + " = " + (n1 + n2));
-               break;
-               case 2:
-                   System.out.println("Çıkarma İşleminin Sonucu = " + n1 + " - " + n2 + " = " + (n1 - n2));
-                   break;
-                   case 3:
-                       System.out.println("Çarpma İşleminin Sonucu = " + n1 + " * " + n2 + " = " + (n1 * n2));
-                       break;
-                       case 4:
-                           System.out.println("Bölme İşleminin Sonucu = " + n1 + " / " + n2 + " = " + (n1 / n2));
+       if (username.equals("admin") && password.equals("admin123")) {
+           System.out.println("Hoşgeldin Admin!");
+       }
+       else {
+           System.out.println("Şifre veya kullanıcı adı yanlış");
        }
     }
 }
