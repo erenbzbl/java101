80e99c53e608adeddba5e7bb504c7f1f80af38d0
erenbzbl <erenbozbel@gmail.com>
2024-12-25 20:07:43 +0300

    harmnik sayılar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,50 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // ********* Girilen Değerin basamaklarının toplamı *********
+        System.out.print("Bir sayı giriniz: ");
+        double sayi = scanner.nextInt();
+        double toplam = 0;
 
-        /*System.out.print("Bir Sayı Giriniz: ");
-        int number = scanner.nextInt();
-        int toplam = 0;
-
-        while (number != 0) {
-            int digit = number % 10;
-            toplam += digit;
-            number = number / 10;
+        for (double i = 1; i <= sayi; i++) {
+            toplam += 1.0 / i;
             System.out.println(toplam);
-        } */
-
-        // ********* - Girilen Değerin basamaklarının toplamı bitiş-  *********
-
-
-        for (int x =1; x <= 999; x++)
-        {
-          int number = x;
-          int toplam = 0;
-          int basamakSayi = 0;
-
-          int  tempNumber = x;
-          while (tempNumber > 0) {
-              tempNumber = tempNumber / 10;
-              basamakSayi++;
-          }
-
-          int sumNumber = x;
-          while (sumNumber > 0) {
-               int  basamak = sumNumber % 10;
-               int  us = 1;
-
-               for(int i = 0; i < basamakSayi; i++ ){
-                   us = basamak * us;
-               }
-
-               toplam =toplam + us;
-               sumNumber = sumNumber / 10;
-          }
-
-          if (toplam == number){
-              System.out.println(number + " armstrong sayıdır" );
-          }
         }
+
+        System.out.println("Harmonik Toplam: " + toplam); // Sonucu yazdır
     }
 }
