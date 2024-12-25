b37a3d6030af303133764d742a45aef6645457b3
erenbzbl <erenbozbel@gmail.com>
2024-12-25 16:17:31 +0300

    armStrong

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,27 +3,53 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static int factorial(int num) {
-        int result = 1;
-        for (int i = 1; i <= num; i++) {
-            result *= i;
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n,k;
 
-        System.out.print("Üssü alınacak sayıyı giriniz: ");
-        n = scanner.nextInt();
-        System.out.print("Üssü alınacak sayıyı çarpan sayıyı giriniz: ");
-        k = scanner.nextInt();
+        // ********* Girilen Değerin basamaklarının toplamı *********
 
-        int total =1;
-        for (int i = 1; i <= k; i++) {
-           total *= n;
+        /*System.out.print("Bir Sayı Giriniz: ");
+        int number = scanner.nextInt();
+        int toplam = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            toplam += digit;
+            number = number / 10;
+            System.out.println(toplam);
+        } */
+
+        // ********* - Girilen Değerin basamaklarının toplamı bitiş-  *********
+
+
+        for (int x =1; x <= 999; x++)
+        {
+          int number = x;
+          int toplam = 0;
+          int basamakSayi = 0;
+
+          int  tempNumber = x;
+          while (tempNumber > 0) {
+              tempNumber = tempNumber / 10;
+              basamakSayi++;
+          }
+
+          int sumNumber = x;
+          while (sumNumber > 0) {
+               int  basamak = sumNumber % 10;
+               int  us = 1;
+
+               for(int i = 0; i < basamakSayi; i++ ){
+                   us = basamak * us;
+               }
+
+               toplam =toplam + us;
+               sumNumber = sumNumber / 10;
+          }
+
+          if (toplam == number){
+              System.out.println(number + " armstrong sayıdır" );
+          }
         }
-        System.out.println(total);
     }
 }
