3302523964f9fdfe28f5d5fcd243771d251408f1
erenbzbl <erenbozbel@gmail.com>
2024-12-27 16:39:51 +0300

    ters üçgen uygulaması

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,16 +7,11 @@ public class Main {
         System.out.print("Bir Sayı Giriniz : ");
         int sayi = scanner.nextInt();
 
-        int toplam = 0;
-        for (int i = 1; i < sayi; i++) {
-            if (sayi % i == 0) {
-                toplam += i;
+        for (int i = sayi; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
             }
-        }
-        if (toplam == sayi) {
-            System.out.println(sayi + " mükemmel sayıdır.");
-        } else {
-            System.out.println(sayi + " mükemmel sayı değildir.");
+            System.out.println();
         }
     }
 }
