4fb774d4f0eda8fbbd83a6c725845f550acdbc3b
erenbzbl <erenbozbel@gmail.com>
2025-01-01 23:36:20 +0300

    5 artırma ve silme

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,24 @@
+package org.example;
+
 import java.util.Scanner;
+
 public class Main {
+    static void number(int sayi){
+        System.out.print(sayi + " ");
+
+        if(sayi > 0){
+            number(sayi - 5);
+            System.out.print(sayi + " ");
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Bir sayı giriniz: ");
         int sayi = scanner.nextInt();
 
-        if (sayi <= 1) {
-            System.out.println(sayi + "sayısı ASAL değildir.");
-        } else {
-            if (asalKontrol(sayi, 2)) {
-                System.out.println(sayi + " sayısı ASALDIR.");
-            } else {
-                System.out.println(sayi + " sayısı ASAL değildir.");
-            }
-        }
-    }
-
-    static boolean asalKontrol(int sayi, int bolen) {
-        if (bolen * bolen > sayi) {
-            return true;
-        }
-        if (sayi % bolen == 0) {
-            return false;
-        }
-        return asalKontrol(sayi, bolen + 1);
+       number(sayi);
     }
 }
+
+
+
