bb17e76b140f01244b0b9d2de9daa64e7f6cfb1a
erenbzbl <erenbozbel@gmail.com>
2025-01-01 22:57:04 +0300

    asal sayı mı değil mi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,192 +1,28 @@
 import java.util.Scanner;
-
 public class Main {
-
-    static void plus() {
-        Scanner scan = new Scanner(System.in);
-        int number, result = 0, i = 1;
-        while (true) {
-            System.out.print(i++ + ". sayı :");
-            number = scan.nextInt();
-            if (number == 0) {
-                break;
-            }
-            result += number;
-        }
-        System.out.println("Sonuç : " + result);
-    }
-
-    static void minus() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Kaç adet sayı gireceksiniz :");
-        int counter = scan.nextInt();
-        int number, result = 0;
-
-        for (int i = 1; i <= counter; i++) {
-            System.out.print(i + ". sayı :");
-            number = scan.nextInt();
-            if (i == 1) {
-                result += number;
-                continue;
-            }
-            result -= number;
-        }
-
-        System.out.println("Sonuç : " + result);
-    }
-
-    static void times() {
-        Scanner scan = new Scanner(System.in);
-        int number, result = 1, i = 1;
-
-        while (true) {
-            System.out.print(i++ + ". sayı :");
-            number = scan.nextInt();
-
-            if (number == 1)
-                break;
-
-            if (number == 0) {
-                result = 0;
-                break;
-            }
-            result *= number;
-        }
-
-        System.out.println("Sonuç : " + result);
-    }
-
-    static void divided() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Kaç adet sayı gireceksiniz :");
-        int counter = scan.nextInt();
-        double number, result = 0.0;
-
-        for (int i = 1; i <= counter; i++) {
-            System.out.print(i + ". sayı :");
-            number = scan.nextDouble();
-            if (i != 1 && number == 0) {
-                System.out.println("Böleni 0 giremezsiniz.");
-                continue;
-            }
-            if (i == 1) {
-                result = number;
-                continue;
-            }
-            result /= number;
-        }
-
-        System.out.println("Sonuç : " + result);
-    }
-
-    static void power() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Taban değeri giriniz :");
-        int base = scan.nextInt();
-        System.out.print("Üs değeri giriniz :");
-        int exponent = scan.nextInt();
-        int result = 1;
-
-        for (int i = 1; i <= exponent; i++) {
-            result *= base;
-        }
-
-        System.out.println("Sonuç : " + result);
-    }
-
-    static void factorial() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Sayı giriniz :");
-        int n = scan.nextInt();
-        int result = 1;
-
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-
-        System.out.println("Sonuç : " + result);
-    }
-
-    static void modeRetrieval() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Bölünen sayıyı giriniz :");
-        int a = scan.nextInt();
-        System.out.print("Bölen sayıyı giriniz :");
-        int b = scan.nextInt();
-        int result = a % b;
-        System.out.println("Sonuç :" + result);
-
-    }
-    static int areaCalculation() {
-
-
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Lütfen kısa kenar giriniz: ");
-        int a = scan.nextInt();
-        System.out.print("Lütfen uzun kenar giriniz: ");
-        int b = scan.nextInt();
-
-
-
-
-        if (a > b) {
-            System.out.println("Kısa kenar uzun kenardan uzun olamaz!");
-            return areaCalculation();
-        } else {
-            int result = a * b;
-            int  environment = a + b;
-            System.out.println("Dikdörtgen Alanı: " + result + "\nÇevresi: " + environment) ;
-        }
-        return 0;
-    }
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int select;
-        String menu = "1- Toplama İşlemi\n"
-                + "2- Çıkarma İşlemi\n"
-                + "3- Çarpma İşlemi\n"
-                + "4- Bölme işlemi\n"
-                + "5- Üslü Sayı Hesaplama\n"
-                + "6- Faktoriyel Hesaplama\n"
-                + "7- Mod Alma\n"
-                + "8- Dikdörtgen Alan ve Çevre Hesabı\n"
-                + "0- Çıkış Yap";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = scanner.nextInt();
 
-        do {
-            System.out.println(menu);
-            System.out.print("Lütfen bir işlem seçiniz :");
-            select = scan.nextInt();
-            switch (select) {
-                case 1:
-                    plus();
-                    break;
-                case 2:
-                    minus();
-                    break;
-                case 3:
-                    times();
-                    break;
-                case 4:
-                    divided();
-                    break;
-                case 5:
-                    power();
-                    break;
-                case 6:
-                    factorial();
-                    break;
-                case 7:
-                    modeRetrieval();
-                    break;
-                    case 8:
-                        areaCalculation();
-                        break;
-                case 0:
-                    break;
-                default:
-                    System.out.println("Yanlış bir değer girdiniz, tekrar deneyiniz.");
+        if (sayi <= 1) {
+            System.out.println(sayi + "sayısı ASAL değildir.");
+        } else {
+            if (asalKontrol(sayi, 2)) {
+                System.out.println(sayi + " sayısı ASALDIR.");
+            } else {
+                System.out.println(sayi + " sayısı ASAL değildir.");
             }
-        } while (select != 0);
+        }
+    }
+
+    static boolean asalKontrol(int sayi, int bolen) {
+        if (bolen * bolen > sayi) {
+            return true;
+        }
+        if (sayi % bolen == 0) {
+            return false;
+        }
+        return asalKontrol(sayi, bolen + 1);
     }
 }
